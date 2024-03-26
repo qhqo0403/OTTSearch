8c060d517f0c2bfbc0243d572aaf97a653a4b897
qhqo0403 <119954728+qhqo0403@users.noreply.github.com>
2024-03-26 20:09:19 +0900

    test

--- src/main/java/com/mySpring/myapp/like/service/LikeService.java
+++
@@ -1,8 +0,0 @@
-package com.mySpring.myapp.like.service;
-
-public interface LikeService {
-    void addLike(int contentId, int userId) throws Exception;
-    void removeLike(int contentId, int userId) throws Exception;
-    int getLikeCount(int contentId) throws Exception;
-    boolean existsLike(int contentId, int userId) throws Exception;
-}
